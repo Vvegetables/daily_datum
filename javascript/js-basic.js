5df7b98b0ba208e432cc57076eccfab07eb03c3d
Vvegetables <625736074@qq.com>
2018-06-03 00:47:19 +0800

    2018-06-03

---
+++ javascript/js-basic.js
@@ -0,0 +1,22 @@
+/*
+1.
+*/
+// js在页面中的位置？不同位置浏览器解释的顺序不一样
+
+document.write('this') //整个html dom重写
+
+/*
+2.
+*/
+//js 变量，var name; js变量的作用域很重要。没有var 声明的变量为全局变量，自动上升。
+
+/*
+3.
+*/
+//js内置弹窗
+alert('内置弹窗');
+var _comfirm = comfirm('请确定') //return true or false
+var _prompt = prompt('请输入') //return the input value
+
+
+
